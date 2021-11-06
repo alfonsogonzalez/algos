7980b49860aa3da4b236031ff0fca41a69e90f6e
alfonso <alfonso8gonzalez@gmail.com>
2021-11-06 16:43:42 -0700

    Add swap_nodes_pairs Python implementation and unit tests

---
+++ src/py/swap_nodes_pairs_test.py
@@ -0,0 +1,44 @@
+'''
+algos by Alfonso Gonzalez
+https://github.com/alfonsogonzalez/algos
+https://www.youtube.com/c/AlfonsoGonzalezSpaceEngineering
+
+Tests of swap_nodes_pairs function
+and ListNode class
+'''
+
+from swap_nodes_pairs import swap_nodes_pairs, ListNode
+
+def test_ListNode_basic_construction():
+	node = ListNode( 3 )
+	assert node.val == 3
+	assert node.next is None
+
+	node4 = ListNode()
+	node3 = ListNode( 3, node4 )
+	node2 = ListNode( 2, node3 )
+	node1 = ListNode( 1, node2 )
+
+	assert node4.val == 0
+	assert node3.val == 3
+	assert node2.val == 2
+	assert node1.val == 1
+
+	assert node4.next is None
+	assert node3.next is node4
+	assert node2.next is node3
+	assert node1.next is node2
+
+def test_swap_nodes_pairs_basic_usage():
+	node4 = ListNode( 4 )
+	node3 = ListNode( 3, node4 )
+	node2 = ListNode( 2, node3 )
+	node1 = ListNode( 1, node2 )
+
+	swap_nodes_pairs( node1 )
+
+	assert node1.val == 2
+	assert node2.val == 1
+	assert node3.val == 4
+	assert node4.val == 3
+	assert node4.next is None	
